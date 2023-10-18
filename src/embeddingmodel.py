eeb449a67f142b148bf5bae4feb384b8577318bc
Finn Lueth <38927318+finnlueth@users.noreply.github.com>
2023-10-18 16:07:08 +0200

    trian on embeddings

--- src/embeddingmodel.py
+++ src/embeddingmodel.py
@@ -2,13 +2,13 @@ import torch
 from torch import nn
 
 
-class LSTMModel(nn.Module):
-    def __init__(self, input_dim, hidden_dim, output_dim):
-        super().__init__()
-        self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True)
-        self.linear = nn.Linear(hidden_dim, output_dim)
+# class LSTMModel(nn.Module):
+#     def __init__(self, input_dim, hidden_dim, output_dim):
+#         super().__init__()
+#         self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True)
+#         self.linear = nn.Linear(hidden_dim, output_dim)
 
-    def forward(self, x):
-        _, (h, _) = self.lstm(x)
-        out = self.linear(h[-1])
-        return out
+#     def forward(self, x):
+#         _, (h, _) = self.lstm(x)
+#         out = self.linear(h[-1])
+#         return out
